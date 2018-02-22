4b8c2b0377002c043a613cc38de94262d51ba2d5
brad <brad.hancock@modernservicesinc.com>
2018-02-22 16:05:32 -0600

    comments added and changed array format

--- i_love_u_script.sh
+++ i_love_u_script.sh
@@ -1,7 +1,7 @@
 
+
+##name in iMessage
 buddy="ff"
-
-
 
 n=$(cat /usr/share/dict/words | wc -l)
 word=$(cat -n /usr/share/dict/words | grep -w $(jot -r 1 1 $n) | cut -f2)
@@ -9,18 +9,18 @@ word=$(cat -n /usr/share/dict/words | grep -w $(jot -r 1 1 $n) | cut -f2)
 ## if you want a random word 
 ## use $word 
 
+## possible words you might want to use to make it not seem like you scripted a message to her 
 
-lovewords[0]="Sugar booty"
-lovewords[1]="Perfect Booty"
-lovewords[2]="Big Booty"
-lovewords[3]="baby baby"
-lovewords[4]="pefect face"
+lovewords=("Sugar Bottom" "Pefect Face" "Round bottom" "Beautiful" "Baby" "Sapphire")
 
-rand=$[ $RANDOM % 5 ]
+## count the keys in the array about and ask bash for a random number
+rand=$[ $RANDOM % ${#lovewords[@]} ]
 
+## provide word from array
 loveword=${lovewords[$rand]}
 
-message="God I love you, $loveword"
+## template for message 
+message="I love you, $loveword"
 
 cmd='tell application "Messages" to send "'$message'" to buddy "'$buddy'"'
 
